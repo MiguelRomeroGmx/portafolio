4e286dd85e5338ef07dc8b23c94cc5ee690bacdc
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-12 20:53:06 -0600

    Pagina de producto

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -21,9 +21,13 @@ cargando:boolean = true;
           this.http.get('https://paginaweb-afe52.firebaseio.com/productos_idx.json')
               .subscribe( res => {
 
-                console.log( res.json() );
-                this.cargando = false;
-                this.productos = res.json();
-              });
+                // console.log( res.json() );
 
+                setTimeout(()=>{
+
+                  this.cargando = false;
+                  this.productos = res.json();
+                },1500 )
+
+              });
    }
