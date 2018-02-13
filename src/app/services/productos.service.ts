a6624aa3b43b766a9ed5be862722d3afe13cf6ea
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-13 12:31:50 -0600

    Primera versión

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,12 +6,53 @@ import { Http }  from "@angular/http";
 export class ProductosService {
 
 productos:any[] = [];
+productos_filtrado:any[] = [];
 cargando:boolean = true;
 
   constructor(private http:Http ) {
 
     this.cargar_productos();
 
+
+    }
+
+    public buscar_producto( termino:string ){
+
+      // console.log("Buscando Producto");
+      // console.log( this.productos.length );
+
+      if ( this.productos.length === 0 ){
+
+          this.cargar_productos().then( ()=>{
+
+          this.filtrar_productos(termino);
+
+          });
+
+      }else{
+        this.filtrar_productos(termino);
+
+      }
+
+    }
+
+    private filtrar_productos(termino:string){
+
+      this.productos_filtrado = [];
+
+      termino = termino.toLowerCase();
+
+      this.productos.forEach( prod =>{
+
+        if( prod.categoria.indexOf( termino )>=0 || prod.titulo.toLowerCase().indexOf( termino )>=0  ){
+          this.productos_filtrado.push( prod );
+          // console.log(prod);
+
+        }
+
+
+        // console.log( prod );
+      })
 
     }
 
@@ -23,18 +64,28 @@ cargando:boolean = true;
     public cargar_productos(){
 
           this.cargando = true;
-          this.http.get('https://paginaweb-afe52.firebaseio.com/productos_idx.json')
-              .subscribe( res => {
 
-                // console.log( res.json() );
+          let promesa = new Promise( ( resolve, reject )=>{
 
-                setTimeout(()=>{
+            this.http.get('https://paginaweb-afe52.firebaseio.com/productos_idx.json')
+                .subscribe( res => {
 
-                  this.cargando = false;
-                  this.productos = res.json();
-                },1500 )
+                  // console.log( res.json() );
 
-              });
+                  // setTimeout(()=>{
+
+                    this.cargando = false;
+                    this.productos = res.json();
+                    resolve();
+                  // },1500 )
+
+                });
+
+          });
+
+          return promesa;
+
+
    }
 
 
